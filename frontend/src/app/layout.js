54112a8afeb47f1db95eeefe60f96ec32d5a6bb2
Al Muhimen <almuhimen4@gmail.com>
2025-12-17 14:51:13 +0600

    Initial Commit 📋

---
+++ frontend/src/app/layout.js
@@ -0,0 +1,23 @@
+import { JetBrains_Mono } from "next/font/google";
+import "./globals.css";
+
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-jetbrains-mono',
+});
+
+export const metadata = {
+  title: "SuperSnipptes",
+  description: "Simplified Codebook Generation Tool For Competitive Programmers",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${jetbrainsMono.variable} font-mono antialiased`}> 
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ frontend/src/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+	return (
+		<div className="flex flex-col items-center justify-center h-screen gap-4 text-center">
+			<h1 className="text-6xl font-bold">Hello and Welcome</h1>
+			<h2 className="text-3xl font-bold">Hope You Will Have a Nice Stay!</h2>
+			<h3 className="text-lg font-italic">So, how is your day going?</h3>
+		</div>
+  	);
+}
