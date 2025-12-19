e4000d98fabd73a87258df12a92b8f454b8fd170
Al Muhimen <almuhimen4@gmail.com>
2025-12-19 18:54:15 +0600

    Added necessary pages

---
+++ frontend/src/app/signup/page.js
@@ -0,0 +1,7 @@
+export default function SignUp() {
+    return (
+        <div className="flex items-center justify-center text-center flex-col h-screen">
+            <h1>This will be the Sign Up Page</h1>
+        </div>
+    );
+}
