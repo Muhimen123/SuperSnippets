bf5ee56fc329240d20d23090766bf7b3b8fe802c
Al Muhimen <almuhimen4@gmail.com>
2025-12-25 19:42:06 +0600

    Added Hamburger menu

--- frontend/src/app/components/NavBar.js
+++ frontend/src/app/components/NavBar.js
@@ -1,14 +1,33 @@
+"use client";
+
 import Link from "next/link";
 import Logo from "./Logo";
 import NavButton from "./NavButton";
 import IconButton from "./IconButton";
+import { useState } from "react";
+
+const navLink = [
+  { key: 1, name: "Home", href: "/" },
+  { key: 2, name: "Features", href: "/" },
+  { key: 3, name: "About Us", href: "/" },
+  { key: 4, name: "Contribute", href: "/" },
+];
 
 export default function Navbar() {
+  return (
+    <div>
+      <DesktopNavbar />
+      <MobileNavbar />
+    </div>
+  );
+}
+
+function DesktopNavbar() {
   return (
     <nav
       className={`
 			fixed top-0 w-full z-50 
-      flex justify-between items-center p-5
+      hidden md:flex justify-between items-center p-5
       bg-white/0 backdrop-blur-xs 
       border-b border-white/0	
 		`}
@@ -17,6 +36,31 @@ export default function Navbar() {
       <PageNavigation />
       <GithubLink />
     </nav>
+  );
+}
+
+function MobileNavbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <nav
+      className={`
+      fixed top-0 w-full z-50
+      md:hidden flex justify-between items-center p-5
+      bg-white/0 backdrop-blur-xs 
+      border-b border-white/0
+    `}
+    >
+      <LogoSection />
+      <HamBurgerMenu />
+    </nav>
+  );
+}
+
+function HamBurgerMenu() {
+  return (
+    <button>
+      <img height={24} width={24} src="icons/hamburger-menu.svg" />
+    </button>
   );
 }
 
@@ -33,14 +77,15 @@ function PageNavigation() {
     <div
       className={`
 			flex
-			justify-center
+			justify-center items-center
 			gap-2
 		`}
     >
-      <NavButton href="/">Home</NavButton>
-      <NavButton href="/">Features</NavButton>
-      <NavButton href="/">About Us</NavButton>
-      <NavButton href="/">Contribute</NavButton>
+      {navLink.map((link) => (
+        <NavButton key={link.key} href={link.href}>
+          {link.name}
+        </NavButton>
+      ))}
     </div>
   );
 }
