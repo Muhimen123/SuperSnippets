5f21247c070a175a22a223e1f5312d5bee55dd70
Al Muhimen <almuhimen4@gmail.com>
2025-12-25 18:32:00 +0600

    Fixed class naming convention, now uses {}

--- frontend/src/app/components/NavBar.js
+++ frontend/src/app/components/NavBar.js
@@ -6,12 +6,12 @@ import IconButton from "./IconButton";
 export default function Navbar() {
   return (
     <nav
-      className="
+      className={`
 			fixed top-0 w-full z-50 
       flex justify-between items-center p-5
       bg-white/0 backdrop-blur-xs 
       border-b border-white/0	
-		"
+		`}
     >
       <LogoSection />
       <PageNavigation />
@@ -30,28 +30,32 @@ function LogoSection() {
 
 function PageNavigation() {
   return (
-    <div className="
+    <div
+      className={`
 			flex
 			justify-center
 			gap-2
-		">
-			<NavButton href="/">Home</NavButton>
-			<NavButton href="/">Features</NavButton>
-			<NavButton href="/">About Us</NavButton>
-			<NavButton href="/">Contribute</NavButton>
+		`}
+    >
+      <NavButton href="/">Home</NavButton>
+      <NavButton href="/">Features</NavButton>
+      <NavButton href="/">About Us</NavButton>
+      <NavButton href="/">Contribute</NavButton>
     </div>
   );
 }
 
 function GithubLink() {
-	return (
-		<div className="
+  return (
+    <div
+      className={`
       flex
       justify-center items-center
       gap-2
-    ">
-			<NavButton href="/login">Log in</NavButton>
-			<IconButton>Star on Github</IconButton>
-		</div>
-	);
+    `}
+    >
+      <NavButton href="/login">Log in</NavButton>
+      <IconButton>Star on Github</IconButton>
+    </div>
+  );
 }
