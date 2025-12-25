e9fcc4e75fc5d9e9bef50407c811953740b06dbd
Al Muhimen <almuhimen4@gmail.com>
2025-12-25 21:16:17 +0600

    Fixed title text scaling on mid screen

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -16,14 +16,14 @@ export default function Home() {
 
 function TitleSection() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <div className="text-5xl font-bold lg:text-9xl flex flex-col items-center justify-center">
+    <div className={`flex flex-col items-center justify-center min-h-screen`}>
+      <div className={`font-bold text-5xl md:text-7xl lg:text-9xl flex flex-col items-center justify-center`}>
         <h1>The Perfect</h1>
         {/* TODO: Add hover effect, change it to C0deB00k and create an underline */}
         <h1>CodeBook</h1>{" "}
         <h1>Made Simple</h1>
       </div>
-      <p className="mt-6 lg:text-xl max-w-4xl text-center">
+      <p className="mt-6 text-md md:text-lg lg:text-xl max-w-4xl text-center">
         Easily generate codebooks from Github repositories or bring your own
         code. Spend more time using the codebook than creating it!
       </p>
