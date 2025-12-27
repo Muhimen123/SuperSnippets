f551601cf2d1f72725a30ed68a141146eae2d53f
Al Muhimen <almuhimen4@gmail.com>
2025-12-27 20:00:41 +0600

    Toolbarelement added hover animation

--- frontend/src/app/editor/components/Toolbar.js
+++ frontend/src/app/editor/components/Toolbar.js
@@ -7,10 +7,10 @@ export default function Toolbar() {
     { title: "Download", icon: "icons/download.svg" },
   ];
 
-	const secondaryTools = [
+  const secondaryTools = [
     { title: "Home", icon: "icons/home.svg" },
     { title: "Settings", icon: "icons/settings.svg" },
-	];
+  ];
 
   return (
     <div
@@ -20,7 +20,7 @@ export default function Toolbar() {
         <ToolbarElement key={tool.title} icon={tool.icon} title={tool.title} />
       ))}
 
-			<div className={`flex-1`}></div>
+      <div className={`flex-1`}></div>
 
       {secondaryTools.map((tool) => (
         <ToolbarElement key={tool.title} icon={tool.icon} title={tool.title} />
@@ -33,12 +33,27 @@ function ToolbarElement({ icon, title, selected = false }) {
   return (
     <div
       className={`
-			flex flex-col items-center justify-center 
-			bg-black text-white p-3 gap-2
-		`}
+				flex flex-col items-center justify-center 
+				w-full bg-black text-white 
+				p-3 gap-2 group cursor-pointer
+			`}
     >
-      <img src={icon} height={30} width={30} />
-      <h6 className={`text-center text-sm`}>{title}</h6>
+      <div
+        className={`
+          absolute left-0 w-2 bg-white transition-all duration-300 rounded
+          ${selected ? "h-10 opacity-100" : "h-0 opacity-0"}
+					group-hover:h-10 group-hover:opacity-50
+        `}
+      ></div>
+
+      <img
+        src={icon}
+        className="brightness-0 invert group-active:scale-95"
+        height={30}
+        width={30}
+        alt={title}
+      />
+      <h6 className="text-center text-xs font-medium group-active:scale-95">{title}</h6>
     </div>
   );
 }
