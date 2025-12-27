8f1b70f896954d6445fb2a052bc0d3e53289427e
Al Muhimen <almuhimen4@gmail.com>
2025-12-27 20:47:06 +0600

    Toolbar State management

--- frontend/src/app/editor/components/Toolbar.js
+++ frontend/src/app/editor/components/Toolbar.js
@@ -1,15 +1,15 @@
-export default function Toolbar() {
+export default function Toolbar({ currentTool, handleToolSelection }) {
   const primaryTools = [
-    { title: "Code Segments", icon: "icons/code-segment.svg" },
-    { title: "Category", icon: "icons/categories.svg" },
-    { title: "Github Repos", icon: "icons/github-repos.svg" },
-    { title: "Configure", icon: "icons/configure.svg" },
-    { title: "Download", icon: "icons/download.svg" },
+    { key: 1, title: "Code Segments", icon: "icons/code-segment.svg" },
+    { key: 2, title: "Category", icon: "icons/categories.svg" },
+    { key: 3, title: "Github Repos", icon: "icons/github-repos.svg" },
+    { key: 4, title: "Configure", icon: "icons/configure.svg" },
+    { key: 5, title: "Download", icon: "icons/download.svg" },
   ];
 
   const secondaryTools = [
-    { title: "Home", icon: "icons/home.svg" },
-    { title: "Settings", icon: "icons/settings.svg" },
+    { key: 6, title: "Home", icon: "icons/home.svg" },
+    { key: 7, title: "Settings", icon: "icons/settings.svg" },
   ];
 
   return (
@@ -17,19 +17,40 @@ export default function Toolbar() {
       className={`flex flex-col items-center w-[100px] p-5 bg-black min-h-screen`}
     >
       {primaryTools.map((tool) => (
-        <ToolbarElement key={tool.title} icon={tool.icon} title={tool.title} />
+        <ToolbarElement
+          key={tool.key}
+          toolKey={tool.key}
+          icon={tool.icon}
+          title={tool.title}
+          currentTool={currentTool}
+          handleToolSelection={handleToolSelection}
+        />
       ))}
 
       <div className={`flex-1`}></div>
 
       {secondaryTools.map((tool) => (
-        <ToolbarElement key={tool.title} icon={tool.icon} title={tool.title} />
+        <ToolbarElement
+          key={tool.key}
+          toolKey={tool.key}
+          icon={tool.icon}
+          title={tool.title}
+          currentTool={currentTool}
+          handleToolSelection={handleToolSelection}
+        />
       ))}
     </div>
   );
 }
 
-function ToolbarElement({ icon, title, selected = false }) {
+function ToolbarElement({
+  toolKey,
+  icon,
+  title,
+  currentTool,
+  handleToolSelection,
+}) {
+  const selected = toolKey === currentTool;
   return (
     <div
       className={`
@@ -37,12 +58,16 @@ function ToolbarElement({ icon, title, selected = false }) {
 				w-full bg-black text-white 
 				p-3 gap-2 group cursor-pointer
 			`}
+      onClick={() => {
+        console.log(`Called from handle ${toolKey}`);
+        handleToolSelection(toolKey);
+      }}
     >
       <div
         className={`
-          absolute left-0 w-2 bg-white transition-all duration-300 rounded
+          absolute left-0 w-1 bg-white transition-all duration-300 rounded-r
           ${selected ? "h-10 opacity-100" : "h-0 opacity-0"}
-					group-hover:h-10 group-hover:opacity-50
+					group-hover:h-10 group-hover:${selected ? "opacity-100" : "opacity-50"}
         `}
       ></div>
 
@@ -53,7 +78,9 @@ function ToolbarElement({ icon, title, selected = false }) {
         width={30}
         alt={title}
       />
-      <h6 className="text-center text-xs font-medium group-active:scale-95">{title}</h6>
+      <h6 className="text-center text-xs font-medium group-active:scale-95">
+        {title}
+      </h6>
     </div>
   );
 }
--- frontend/src/app/editor/page.js
+++ frontend/src/app/editor/page.js
@@ -1,9 +1,25 @@
+"use client";
+
+import { useState } from "react";
 import Toolbar from "./components/Toolbar";
 
 export default function PDFEditor() {
+  const [currentTool, setCurrentTool] = useState(1);
+
+  const handleToolSelection = (toolKey) => {
+    console.log(`from actual ${toolKey}`);
+    setCurrentTool(toolKey);
+  };
+
   return (
-		<div>
-			<Toolbar/>
-		</div>
+    <div className="flex">
+      <Toolbar
+        currentTool={currentTool}
+        handleToolSelection={handleToolSelection}
+      />
+      <h1 className="flex flex-row items-center justify-center text-8xl">
+        {currentTool}
+      </h1>
+    </div>
   );
 }
