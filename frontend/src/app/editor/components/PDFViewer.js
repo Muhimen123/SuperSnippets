af53cc808e8db034a417c313415a07bec324b0d3
Al Muhimen <almuhimen4@gmail.com>
2025-12-29 23:01:36 +0600

    Added browser default pdf viewer

--- frontend/src/app/editor/components/PDFViewer.js
+++ frontend/src/app/editor/components/PDFViewer.js
@@ -1,5 +1,15 @@
 export default function PDFViewer( {className} ) {
-	return(
-		<h1 className={`${className}`}>Place the pdf here</h1>
-	);
+	const pdfUrl = "https://snippet.embedpdf.com/ebook.pdf";
+
+  return (
+    <div className="h-full w-full overflow-hidden">
+      <iframe
+        src={`${pdfUrl}#toolbar=1`}
+        title="PDF Viewer"
+        className="w-full h-full border-none"
+        // Key for accessibility and functionality
+        allow="fullscreen" 
+      />
+    </div>
+  );
 }
