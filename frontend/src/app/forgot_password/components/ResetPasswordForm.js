d94527e13fc61d27d4ade8c7f7bf5f083bcd3e73
Al Muhimen <almuhimen4@gmail.com>
2025-12-31 04:45:36 +0600

    Added animation to reset password page

--- frontend/src/app/forgot_password/components/ResetPasswordForm.js
+++ frontend/src/app/forgot_password/components/ResetPasswordForm.js
@@ -2,9 +2,9 @@ import Logo from "../../components/Logo";
 import TextField from "../../components/TextField";
 import Link from "next/link";
 
-export default function ResetPasswordForm() {
+export default function ResetPasswordForm({ onSendCodeClick }) {
   return (
-    <div className="w-full lg:w-1/2 flex items-center justify-end px-8 pr-24">
+    <div className="">
       <div className="w-full max-w-xs">
         <div className="mb-8 ml-18">
           <Link href={"/"}>
@@ -29,12 +29,16 @@ export default function ResetPasswordForm() {
             />
           </div>
 
-          <Link
+          <button
             href="/forgot_password/verification"
-            className="w-full bg-black text-white py-3 px-4 rounded-lg hover:bg-gray-800 transition-colors flex items-center justify-center"
+            className={`cursor-pointer w-full bg-black text-white py-3 px-4 rounded-lg hover:bg-gray-800 transition-colors flex items-center justify-center`}
+            onClick={(event) => {
+              event.preventDefault();
+              onSendCodeClick();
+            }}
           >
             Send Code →
-          </Link>
+          </button>
         </form>
       </div>
     </div>
--- frontend/src/app/forgot_password/page.js
+++ frontend/src/app/forgot_password/page.js
@@ -1,17 +1,49 @@
+"use client";
+
 import TileBackground from "../components/TileBackground";
 import DesignSVG from "../components/DesignSVG";
 import ResetPasswordForm from "./components/ResetPasswordForm";
+import { AnimatePresence } from "framer-motion";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { SlideIn } from "../components/Animations";
 
 export default function ResetPassword() {
+  const [isVisible, setIsVisible] = useState(true);
+  const router = useRouter();
+
+  const handleNavigate = () => {
+    setIsVisible(false); // Trigger the exit animation
+    setTimeout(() => {
+      router.push("/forgot_password/verification"); // Navigate after animation completes (0.25s)
+    }, 250);
+  };
+
   return (
     <TileBackground>
       <div className="min-h-screen flex ml-20">
         {/* Left side - Reset Password Form */}
-        <ResetPasswordForm />
+        <div
+          className={`w-full lg:w-1/2 flex items-center justify-end px-8 pr-24`}
+        >
+          <AnimatePresence>
+            {isVisible && (
+              <SlideIn direction={"right"}>
+                <ResetPasswordForm onSendCodeClick={handleNavigate}/>
+              </SlideIn>
+            )}
+          </AnimatePresence>
+        </div>
 
         {/* Right side - Abstract Pattern */}
         <div className="hidden lg:flex lg:w-1/2 items-center justify-left">
-          <DesignSVG className="w-[500px] h-[600px]" />
+          <AnimatePresence>
+            {isVisible && (
+              <SlideIn direction={"left"}>
+                <DesignSVG className="w-[500px] h-[600px]" />
+              </SlideIn>
+            )}
+          </AnimatePresence>
         </div>
       </div>
     </TileBackground>
