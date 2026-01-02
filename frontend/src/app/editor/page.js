c1873f0e6cfbbfe3708027f278b0da0b18adffc0
Al Muhimen <almuhimen4@gmail.com>
2026-01-03 03:58:43 +0600

    Added embedPDF

--- frontend/src/app/editor/page.js
+++ frontend/src/app/editor/page.js
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import Toolbar from "./components/Toolbar";
 import ContentSection from "../editor/components/ContentSection";
-import PDFSection from "./components/PDFSection";
+import { PDFViewer } from "@embedpdf/react-pdf-viewer";
 
 export default function Editor() {
   const [currentTool, setCurrentTool] = useState(1);
@@ -13,18 +13,33 @@ export default function Editor() {
   };
 
   return (
-    <div className="flex h-screen overflow-hidden">
+    // h-screen ensures the app takes the full viewport height
+    <div className="flex h-screen w-screen overflow-hidden bg-gray-50">
       <Toolbar
         currentTool={currentTool}
         handleToolSelection={handleToolSelection}
       />
-      <div className={`flex flex-1 overflow-hidden`}>
-        <div>
-          <ContentSection activeTool={currentTool} handleToolSelection={handleToolSelection} />
+
+      <div className="flex flex-1 overflow-hidden">
+        <div className="shrink-0">
+          <ContentSection
+            activeTool={currentTool}
+            handleToolSelection={handleToolSelection}
+          />
         </div>
-        <div className={`flex-1 relative w-full h-full`}>
-          <PDFSection />
+
+        <div className="relative flex-1 h-full bg-gray-200">
+          <div className="absolute inset-0 z-0">
+            <PDFViewer
+              config={{
+                src: "https://snippet.embedpdf.com/ebook.pdf",
+                theme: { preference: "light" },
+              }}
+              style={{ width: "100%", height: "100%" }}
+            />
+          </div>
         </div>
+
       </div>
     </div>
   );
