6775492cd9aee6090a75f50fe4e948517aefcced
Al Muhimen <almuhimen4@gmail.com>
2026-01-04 00:09:32 +0600

    Added font dropdown

--- frontend/src/app/editor/components/ConfigBar.js
+++ frontend/src/app/editor/components/ConfigBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 
 const Input = (props) => (
   <input
@@ -35,21 +36,42 @@ const Select = ({ children, ...props }) => (
   </div>
 );
 
+const PREDEFINED_FONTS = [
+  "Jetbrains Mono",
+  "Roboto",
+  "Open Sans",
+  "Lato",
+  "Montserrat",
+  "Cascadia Code",
+  "Arial",
+  "Helvetica",
+  "Times New Roman",
+  "Georgia",
+  "Courier New",
+  "Verdana",
+];
+
 export default function ConfigBar() {
+  const [fonts, setFonts] = useState(PREDEFINED_FONTS);
+
   return (
     <div className="flex flex-col h-full w-80 border-r-2 border-black bg-white text-black font-mono p-6 overflow-y-auto gap-6">
       {/* Font */}
       <div className="flex flex-col gap-2">
         <label className="font-bold text-sm">Font</label>
         <Select>
-          <option>Placeholder</option>
+          {fonts.map((font, index) => (
+            <option key={index} value={font} style={{ fontFamily: font }}>
+              {font}
+            </option>
+          ))}
         </Select>
       </div>
 
       {/* Font Size */}
       <div className="flex flex-col gap-2">
         <label className="font-bold text-sm">Font Size</label>
-        <Input type="number" placeholder="Placeholder" />
+        <Input type="number" placeholder="11" />
       </div>
 
       {/* Header Text */}
@@ -60,27 +82,27 @@ export default function ConfigBar() {
         </div>
         <textarea
           className="w-full bg-[#E5E5E5] text-black p-3 rounded-lg outline-none resize-none h-24 focus:ring-2 focus:ring-black/20 transition-all placeholder-black/40 hover:bg-[#E5E5E5] font-mono"
-          placeholder="Placeholder"
+          placeholder="CodeBook"
           maxLength={100}
         />
       </div>
 
       {/* Margin Size */}
       <div className="flex flex-col gap-2">
-        <label className="font-bold text-sm">Margin Size</label>
-        <Input type="number" placeholder="Placeholder" />
+        <label className="font-bold text-sm">Margin Size (cm)</label>
+        <Input type="number" placeholder="3" />
       </div>
 
       {/* Columns */}
       <div className="flex flex-col gap-2">
         <label className="font-bold text-sm">Columns</label>
-        <Input type="number" placeholder="Placeholder" />
+        <Input type="number" placeholder="1" />
       </div>
 
       {/* Page Limit */}
       <div className="flex flex-col gap-2">
         <label className="font-bold text-sm">Page Limit</label>
-        <Input type="number" placeholder="Placeholder" />
+        <Input type="number" placeholder="1" />
       </div>
 
       {/* Page Orientation */}
