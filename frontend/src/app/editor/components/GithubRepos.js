23b3f544508c292312a60afda70de9511cd056d1
Al Muhimen <almuhimen4@gmail.com>
2026-01-04 00:46:08 +0600

    Fixed content section width

--- frontend/src/app/editor/components/GithubRepos.js
+++ frontend/src/app/editor/components/GithubRepos.js
@@ -11,7 +11,7 @@ export default function GithubRepos() {
 
   return (
     <>
-      <div className="flex flex-col h-full w-64 border-r-2 border-black bg-white text-black font-mono">
+      <div className="flex flex-col h-full w-80 border-r-2 border-black bg-white text-black font-mono">
         <div className="flex-1 overflow-y-auto">
           {repos.map((repo, index) => (
             <div
