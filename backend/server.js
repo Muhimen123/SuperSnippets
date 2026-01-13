9a3082eaae36912e4e2d9821283f317683942697
Al Muhimen <almuhimen4@gmail.com>
2026-01-13 23:33:39 +0600

    Seperated server logic to app.js

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,8 @@
 import "dotenv/config";
-import express, { json } from "express";
-import cors from "cors";
+import app from "./src/app.js"
 
-const app = express();
+
 const PORT = process.env.PORT;
-
-app.use(cors());
-app.use(json());
-
-app.get("/", (req, res) => {
-  const currentDate = new Date();
-
-  res.status(200).json({
-    message: "Server is up and running!",
-    serverClock: currentDate.toString(),
-  });
-});
 
 app.listen(PORT, () => {
   console.log(`\n=== Listening on: http://localhost:${PORT} ===\n`);
