740a50bbb4ed300e4f4bbbbe4e0b4200f4a1a6b9
Al Muhimen <almuhimen4@gmail.com>
2026-01-14 22:29:55 +0600

    Added pdf routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 import express, { json } from "express";
 import cors from "cors";
+import allRoutes from "./routes/index.js";
 
 const app = express();
 
 app.use(cors());
 app.use(json());
+app.use("/api", allRoutes);
 
 app.get("/", (req, res) => {
   const currentDate = new Date();
