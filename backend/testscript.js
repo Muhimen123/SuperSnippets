34e8ffefdb05bf03e4790623b3c16be97310fc14
Al Muhimen <almuhimen4@gmail.com>
2026-02-14 23:46:56 +0600

    Add service to log activity

--- backend/testscript.js
+++ backend/testscript.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import mongoose from "mongoose";
-import { removeCodebook } from "./src/services/pdf.service.js";
+import { logActivity } from "./src/services/activity.service.js";
 
 const MONGODB_URI = process.env.MONGO_URI; 
 
@@ -10,8 +10,26 @@ async function runTest() {
     console.log("Connected to MongoDB...");
 
     // Run test function
-    const result = await removeCodebook("698e31dcaf49d7a1bec73eec");
-    console.log("Removed codebook:", result);
+
+    const prevCodesegment = {
+      title: "Old Code Segment",
+      code: "console.log('Hello, World!');",
+    };
+
+    const newCodesegment = {
+      title: "Updated Code Segment",
+      code: "console.log('Hello, something something!');",
+    };
+
+    await logActivity({
+      userId: "698cb24b1a1c86f156b1ec06",
+      codebookId: "698e2d343b5d0087a13e06c9",
+      codesegmentId: "64b8ca1fe1b2c3d4e5f67892",
+      previousCodesegment: prevCodesegment,
+      newCodesegment: newCodesegment,
+      actionType: "UPDATE",
+    });
+
   } catch (err) {
     console.error("Test failed:", err);
   } finally {
